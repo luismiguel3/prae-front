7f4a84fae6defd89278e8edc37e58ac6abe580c3
Luis Miguel <lmcorrea@inf.ufsm.br>
2023-05-17 16:20:44 -0300

    styling login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import './styles.css';
-import { Form } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
+import { Container, Form, Button, Row, Col, FormGroup, NavLink } from 'react-bootstrap';
 import { z } from 'zod';
 import {zodResolver} from '@hookform/resolvers/zod';
 
@@ -23,33 +22,47 @@ function Login() {
     function onSubmit(data) {
         console.log(data);
     }
-    
+
     return (
-        <div className="container">
-            <Form onSubmit={handleSubmit(onSubmit)}>
-                <Form.Group controlId="Email">
-                    <Form.Label style={{ 
-                        fontWeight: 'bold',
-                        fontSize: '1.3REM', 
-                        marginBottom: '10px'   
-                    }}
-                        >Email</Form.Label>
-                    <Form.Control className='input' {...register('email')}/>
-                    {errors.email && <span>{errors.email.message}</span>}
-                </Form.Group>
-                <Form.Group controlId="Password" >
-                    <Form.Label style={{
-                        marginBottom: '10px',
-                        marginTop: '20px',
-                        fontWeight: 'bold',
-                        fontSize: '1.3REM',
-                    }} >Password </Form.Label>
-                    <Form.Control className='input'  {...register('senha')}/>
-                    {errors.senha && <span>{errors.senha.message}</span>}
-                </Form.Group>
-                <Button variant="dark" type="submit" style={{marginTop: '20px'}}>Entrar</Button>
+        <Container fluid className='d-flex align-items-center justify-content-center' style={{height: '100vh'}}>
+            <Form onSubmit={handleSubmit(onSubmit)} style={{width:'100%'}}>
+                <Row md={1} className="justify-content-center"  > 
+                    <Col md={4}>
+                        <Form.Group controlId="Email">
+                            <Form.Label style={{ 
+                                fontWeight: 'bold',
+                                fontSize: '1.3REM', 
+                                marginBottom: '10px'   
+                            }}
+                                >Email</Form.Label>
+                            <Form.Control className='input' {...register('email')}/>
+                            {errors.email && <span>{errors.email.message}</span>}
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row md={1} className="justify-content-center"  >
+                    <Col md={4}>
+                        <Form.Group controlId="Password" >
+                            <Form.Label style={{
+                                marginBottom: '10px',
+                                marginTop: '20px',
+                                fontWeight: 'bold',
+                                fontSize: '1.3REM',
+                            }} >Senha </Form.Label>
+                            <Form.Control className='input'  {...register('senha')}/>
+                            {errors.senha && <span>{errors.senha.message}</span>}
+                        </Form.Group>
+                        <FormGroup className='text-center'>
+                            <Button variant="dark" type="submit" size="lg" className='px-5 submit-button'style={{marginTop:'20px'}}>Entrar</Button>
+                        </FormGroup>
+                        <FormGroup className='d-flex justify-content-between' style={{marginTop:"20px"}} >
+                            <NavLink href='/cadastro' style={{color: "#1177BB", marginLeft:"30px", fontSize: "17px"}}>Cadastre-se</NavLink>
+                            <NavLink href='/recuperar-senha' style={{color: "#1177BB",fontSize: "17px"}}>Esqueceu a senha?</NavLink>
+                        </FormGroup>
+                    </Col>
+                </Row>
             </Form>
-        </div>
+        </Container>
     );
 }
 

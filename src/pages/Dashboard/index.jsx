f305595eee0235dbd381faa39881aa5bac4690ad
Luis Miguel <lmcorrea@inf.ufsm.br>
2023-06-10 18:48:55 -0300

    dashboard styles and prototype

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -4,59 +4,53 @@ import './styles-dashboard.css';
 import { Container, Form, Button, Row, Col, FormGroup, NavLink, Card, CardGroup} from 'react-bootstrap';
 
 function Dashboard() {
-    //create cards using bootstrap card with 5 cards on each row
-
+    
     return (
-        <div style={{display:"flex", justifyContent:"center",alignItems:"center", marginTop:"5%"}}>
-            <CardGroup style={{width: "50.2vw"}} >
-                <Card> 
-                    <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51lRMYwP-4L.jpg" style={{width:"11vw", height:"auto"}}   />
-                    <Card.Body >
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                <div style={{marginRight:"2vw"}}></div>
-                <Card> 
-                    <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51lRMYwP-4L.jpg" style={{width:"11vw", height:"auto"}}   />
-                    <Card.Body >
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                <div style={{marginRight:"2vw"}}></div>
-                <Card> 
-                    <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51lRMYwP-4L.jpg" style={{width:"11vw", height:"auto"}}   />
-                    <Card.Body >
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                <div style={{marginRight:"2vw"}}></div>
-                <Card> 
-                    <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51lRMYwP-4L.jpg" style={{width:"11vw", height:"auto"}}   />
-                    <Card.Body >
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+        <div style={{display:"flex", border:"1px solid red"}}>
 
-                
+            <aside style={{height:"100vh", width:"18vw",border:"1px solid red", justifyContent:"center",alignItems:"center" }}>
+                <h1 style={{border:"1px solid black"}}>tem q alinhar no meio</h1>
+            </aside>
 
-                
-            </CardGroup>
+            <div style={{alignItems:"start" ,marginLeft:"10%", marginTop:"8%"}}>
+                <CardGroup style={{width: "50.2vw"}} >
+                    <Card> 
+                        <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51lRMYwP-4L.jpg" style={{ height:"auto"}}   />
+                        <Card.Body>
+                            <Card.Text style={{fontSize:"1rem"}} >Harry Potter e a Pedra Filosofal</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <div style={{marginRight:"2vw"}}></div>
+                    <Card> 
+                        <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51lRMYwP-4L.jpg" style={{ height:"auto"}}   />
+                        <Card.Body >
+                        <Card.Text style={{fontSize:"1rem"}} >Harry Potter e a Pedra Filosofal</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <div style={{marginRight:"2vw"}}></div>
+                    <Card> 
+                        <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51lRMYwP-4L.jpg" style={{ height:"auto"}}   />
+                        <Card.Body >
+                        <Card.Text style={{fontSize:"1rem"}} >Harry Potter e a Pedra Filosofal</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <div style={{marginRight:"2vw"}}></div>
+                    <Card> 
+                        <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51lRMYwP-4L.jpg" style={{ height:"auto"}}   />
+                        <Card.Body >
+                        <Card.Text style={{fontSize:"1rem"}} >Harry Potter e a Pedra Filosofal</Card.Text>
+                        </Card.Body>
+                    </Card>   
+                    <div style={{marginRight:"2vw"}}></div>
+                    <Card> 
+                        <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51lRMYwP-4L.jpg" style={{ height:"auto"}}   />
+                        <Card.Body >
+                        <Card.Text style={{fontSize:"1rem"}} >Harry Potter e a Pedra Filosofal</Card.Text>
+                        </Card.Body>
+                    </Card>     
+                             
+                </CardGroup>
+            </div>
         </div>
     )
 
